ad9e00349bd109551b38dd1e64d012699e650ba6
ShrutiKrishna1379 <shrutikrishna.rsvm@gmail.com>
2025-02-06 23:34:35 +0530

    changes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import urlRoutes from './routes/urlRoutes.js';
+import urlRoutes from '../src/routes/urlRoutes.js';
 
 dotenv.config();
 

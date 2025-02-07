07ed71d789a764941bb8e0ae02393c2ed3f72526
ShrutiKrishna1379 <shrutikrishna.rsvm@gmail.com>
2025-02-07 19:02:22 +0530

    changes

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 
 mongoose
     .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
         dbName: 'URL_Shortener_Project',
     })
     .then(() => console.log('Mongodb Connected'))
